6397530165c3fc2fa2eab6881d2b031cead2b117
github username <github email address>
2019-11-11 20:47:38 -0700

    a little more stubbing

--- lib/verily_projects/cli.rb
+++ lib/verily_projects/cli.rb
@@ -1,13 +1,31 @@
 class VerilyProjects::CLI
   def call
-    # list_projects
+
     puts 'Welcome to the verily projects CLI! Which project would you like more information on today?'
-    # get_user_project_selection
-    # get_information_for(project_selection)
-    puts 'Would you like more information about another project?'
-    # get_user_response
-    # logic - if yes, list projects, ask selection, get information, ask again
-    #   if no,
+    get_projects
+    get_user_selection
+    get_user_project
+    puts 'Would you like more information about another project? Please enter y/n.'
+    get_user_response
+    #logic to determine whether to loop through beginning of CLI for new project.
+    # else, continue
     puts "Have a great day!"
   end
+
+  def get_projects
+      @projects = ['fake', 'data', 'to', 'get', 'started']
+  end
+
+  def get_user_selection
+    @user_project = gets.strip
+  end
+
+  def get_user_project
+    #get project info using scraper class
+  end
+
+  def get_user_response
+    @user_response = gets.strip
+  end
+
 end
